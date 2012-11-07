8948b4f8e8d0a19485c105a9214f20d1f39b7e13
Dmitry Melnikov <dmitryme@gmail.com>
2012-11-07 17:59:59 +0400

    Update src/fix_msg.erl

--- src/fix_msg.erl
+++ src/fix_msg.erl
@@ -1,27 +1,12 @@
--module(erlang_fix).
+-module(fix_msg).
 
--export([create_parser/3]).
+-export([create/2]).
 
 -on_load(load_lib/0).
 
--type attr() :: {'page_size', pos_integer()} |
-                {'max_page_size', pos_integer()} |
-                {'num_pages', pos_integer()} |
-                {'max_pages', pos_integer()} |
-                {'num_groups', pos_integer()} |
-                {'max_groups', pos_integer()}.
--type attrs() :: [attr()].
--type flag() :: check_crc |
-                 check_required |
-                 check_value |
-                 check_unknown_fields |
-                 check_all.
--type flags() :: [flag()].
--type parserRef() :: {pos_integer(), binary()}.
-
 load_lib() ->
-   erlang:load_nif(code:priv_dir(erlang_fix) ++ "/erlang_fix", 0).
+   erlang:load_nif(code:priv_dir(erlang_fix) ++ "/fix_msg", 0).
 
--spec create_parser(string(), attrs(), flags()) -> {ok, parserRef()} | {error, string()}.
-create_parser(_Path, _Attrs, _Flags) ->
+-spec create(string(), attrs(), flags()) -> {ok, msgRef()} | {error, reason()}.
+create(parserRef(), string()) ->
    {error, library_not_loaded}.
