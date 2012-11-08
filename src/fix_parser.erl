bfac7515bc8d5813ee62b8696c50d23c147dc19c
Dmitry Melnikov <dmitryme@gmail.com>
2012-11-08 16:22:07 +0400

    Update src/fix_parser.erl

--- src/fix_parser.erl
+++ src/fix_parser.erl
@@ -1,6 +1,6 @@
 -module(fix_parser).
 
--export([create/3]).
+-export([create/3, create_msg/2, add_field/3]).
 
 -on_load(load_lib/0).
 
@@ -18,11 +18,22 @@
                  check_all.
 -type flags() :: [flag()].
 -type parserRef() :: {reference(), binary()}.
--export_type([parserRef/0]).
+-type msgRef() :: {reference(), binary(), binary()}.
+-type reason() :: atom() |
+                  string() |
+                  {pos_integer(), string()}.
 
 load_lib() ->
    erlang:load_nif(code:priv_dir(erlang_fix) ++ "/fix_parser", 0).
 
--spec create(string(), attrs(), flags()) -> {ok, parserRef()} | {error, string()}.
+-spec create(string(), attrs(), flags()) -> {ok, parserRef()} | {error, reason()}.
 create(_Path, _Attrs, _Flags) ->
+   {error, library_not_loaded}.
+
+-spec create_msg(parserRef(), string()) -> {ok, msgRef()} | {error, reason()}.
+create_msg(_ParserRef, _MsgType) ->
+   {error, library_not_loaded}.
+
+-spec add_field(msgRef(), pos_integer(), any()) -> ok | {error, reason()}.
+add_field(_MsgRef, _FieldNum, _Value) ->
    {error, library_not_loaded}.
