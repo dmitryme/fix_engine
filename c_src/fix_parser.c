9703c06906e4ef08580c9ac0112d647b670a08a0
Dmitry Melnikov <dmitryme@gmail.com>
2012-11-08 16:21:37 +0400

    Update c_src/fix_parser.c

--- c_src/fix_parser.c
+++ c_src/fix_parser.c
@@ -1,32 +1,64 @@
-/* @file   fix_parser.h
+/* @file   fix_parser.c
    @author Dmitry S. Melnikov, dmitryme@gmail.com
    @date   Created on: 11/07/2012 17:54:30 AM
 */
-
-#include "common.h"
 
 #include <erl_nif.h>
 #include <linux/limits.h> // for PATH_MAX const
 #include <fix_parser.h>
 #include <fix_error.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 static ERL_NIF_TERM ok_atom;
-static ErlNifResourceType* parsers_res;
+static ERL_NIF_TERM error_atom;
+static ErlNifResourceType* parser_res;
+static ErlNifResourceType* message_res;
 
-static void free_parser(ErlNifEnv* env, void* obj)
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static ERL_NIF_TERM make_error(ErlNifEnv* env, char const* errorMsg, ...)
 {
-   fix_parser_free((FIXParser*)obj);
+   va_list ap;
+   va_start(ap, errorMsg);
+   char text[1024];
+   vsnprintf(text, sizeof(text), errorMsg, ap);
+   return enif_make_tuple2(env, error_atom, enif_make_string(env, text, ERL_NIF_LATIN1));
 }
 
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static ERL_NIF_TERM make_parser_error(ErlNifEnv* env, int errCode, char const* errText)
+{
+   return enif_make_tuple2(
+         env, error_atom, enif_make_tuple2(
+            env, enif_make_int(env, errCode), enif_make_string(env, errText, ERL_NIF_LATIN1)));
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static void free_fix_parser(ErlNifEnv* env, void* obj)
+{
+   fix_parser_free(*(FIXParser**)obj);
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static void free_fix_msg(ErlNifEnv* env, void* obj)
+{
+   fix_msg_free(*(FIXMsg**)obj);
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
 static int load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
 {
-   parsers_res = enif_open_resource_type( env, NULL, "parsers_res",
-      free_parser, ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
+   parser_res = enif_open_resource_type( env, NULL, "erlang_fix_parser_res",
+      free_fix_parser, ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
+   message_res = enif_open_resource_type( env, NULL, "erlang_fix_message_res",
+      free_fix_msg, ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER, NULL);
    ok_atom = enif_make_atom(env, "ok");
+   error_atom = enif_make_atom(env, "error");
    return 0;
 }
 
+/*-----------------------------------------------------------------------------------------------------------------------*/
 static ERL_NIF_TERM create(ErlNifEnv* env, int argc, ERL_NIF_TERM const argv[])
 {
    char path[PATH_MAX] = {};
@@ -89,16 +121,57 @@ static ERL_NIF_TERM create(ErlNifEnv* env, int argc, ERL_NIF_TERM const argv[])
    {
       return make_parser_error(env, get_fix_error_code(), get_fix_error_text());
    }
-   FIXParser** pres = (FIXParser**)enif_alloc_resource(parsers_res, sizeof(FIXParser*));
+   FIXParser** pres = (FIXParser**)enif_alloc_resource(parser_res, sizeof(FIXParser*));
    *pres = parser;
    ERL_NIF_TERM pres_term = enif_make_resource(env, pres);
    enif_release_resource(pres);
    return enif_make_tuple2(env, ok_atom, enif_make_tuple2(env, enif_make_ref(env), pres_term));
 }
 
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static ERL_NIF_TERM create_msg(ErlNifEnv* env, int argc, ERL_NIF_TERM const argv[])
+{
+   int arity;
+   ERL_NIF_TERM const* tuple = NULL;
+   if (!enif_get_tuple(env, argv[0], &arity, &tuple))
+   {
+      return make_error(env, "Wrong parserRef.");
+   }
+   void* res = NULL;
+   if (!enif_get_resource(env, tuple[1], parser_res, &res))
+   {
+      return make_error(env, "Wrong parser resource.");
+   }
+   FIXParser* parser = *(FIXParser**)res;
+   char msgType[12];
+   if (enif_get_string(env, argv[1], msgType, sizeof(msgType), ERL_NIF_LATIN1) <= 0)
+   {
+      return make_error(env, "Wrong msgType.");
+   }
+   FIXMsg* msg = fix_msg_create(parser, msgType);
+   if (!msg)
+   {
+      return make_parser_error(env, get_fix_parser_error_code(parser), get_fix_parser_error_text(parser));
+   }
+   FIXMsg** pmsg = (FIXMsg**)enif_alloc_resource(message_res, sizeof(FIXMsg*));
+   *pmsg = msg;
+   ERL_NIF_TERM pmsg_term = enif_make_resource(env, pmsg);
+   enif_release_resource(pmsg);
+   return enif_make_tuple2(env, ok_atom, enif_make_tuple3(env, enif_make_ref(env), tuple[1], pmsg_term));
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
+static ERL_NIF_TERM add_field(ErlNifEnv* env, int argc, ERL_NIF_TERM const argv[])
+{
+   return ok_atom;
+}
+
+/*-----------------------------------------------------------------------------------------------------------------------*/
 static ErlNifFunc nif_funcs[] =
 {
-   {"create", 3, create}
+   {"create",     3, create},
+   {"create_msg", 2, create_msg},
+   {"add_field",  3, add_field}
 };
 
 ERL_NIF_INIT(fix_parser, nif_funcs, load, NULL, NULL, NULL)
