c92128510b3503ef2b6b11807d873cd0efa59de4
Dmitry Melnikov <dmitryme@gmail.com>
2012-11-12 18:46:41 +0400

    Update src/fix_parser.erl

--- src/fix_parser.erl
+++ src/fix_parser.erl
@@ -1,6 +1,8 @@
 -module(fix_parser).
 
--export([create/3, create_msg/2, set_int_field/3, set_double_field/3, set_string_field/3, set_char_field/3]).
+-export([create/3, create_msg/2,
+      set_int32_field/3, set_int64_field/3, set_double_field/3, set_string_field/3, set_char_field/3,
+      get_int32_field/2, get_int64_field/2, get_double_field/2, get_string_field/2, get_char_field/2]).
 
 -on_load(load_lib/0).
 
@@ -19,6 +21,7 @@
 -type flags() :: [flag()].
 -type parserRef() :: {reference(), binary()}.
 -type msgRef() :: {reference(), binary(), binary()}.
+-type tagNum() :: pos_integer().
 -type reason() :: atom() |
                   string() |
                   {pos_integer(), string()}.
@@ -34,18 +37,42 @@ create(_Path, _Attrs, _Flags) ->
 create_msg(_ParserRef, _MsgType) ->
    {error, library_not_loaded}.
 
--spec set_int_field(msgRef(), pos_integer(), any()) -> ok | {error, reason()}.
-set_int_field(_MsgRef, _FieldNum, _Value) ->
+-spec set_int32_field(msgRef(), tagNum(), integer()) -> ok | {error, reason()}.
+set_int32_field(_MsgRef, _FieldNum, _Value) ->
    {error, library_not_loaded}.
 
--spec set_double_field(msgRef(), pos_integer(), any()) -> ok | {error, reason()}.
-set_double_field(_MsgRef, _FieldNum, _Value) ->
+-spec set_int64_field(msgRef(), tagNum(), integer()) -> ok | {error, reason()}.
+set_int64_field(_MsgRef, _FieldNum, _Value) ->
    {error, library_not_loaded}.
 
--spec set_string_field(msgRef(), pos_integer(), any()) -> ok | {error, reason()}.
-set_string_field(_MsgRef, _FieldNum, _Value) ->
+-spec set_double_field(msgRef(), tagNum(), float()) -> ok | {error, reason()}.
+set_double_field(_MsgRef, _TagNum, _Value) ->
    {error, library_not_loaded}.
 
--spec set_char_field(msgRef(), pos_integer(), any()) -> ok | {error, reason()}.
-set_char_field(_MsgRef, _FieldNum, _Value) ->
+-spec set_string_field(msgRef(), tagNum(), string()) -> ok | {error, reason()}.
+set_string_field(_MsgRef, _TagNum, _Value) ->
+   {error, library_not_loaded}.
+
+-spec set_char_field(msgRef(), tagNum(), char()) -> ok | {error, reason()}.
+set_char_field(_MsgRef, _TagNum, _Value) ->
+   {error, library_not_loaded}.
+
+-spec get_int32_field(msgRef(), tagNum()) -> {ok, integer()} | {error, reason()}.
+get_int32_field(_MsgRef, _TagNum) ->
+   {error, library_not_loaded}.
+
+-spec get_int64_field(msgRef(), tagNum()) -> {ok, integer()} | {error, reason()}.
+get_int64_field(_MsgRef, _TagNum) ->
+   {error, library_not_loaded}.
+
+-spec get_double_field(msgRef(), tagNum()) -> {ok, float()} | {error, reason()}.
+get_double_field(_MsgRef, _TagNum) ->
+   {error, library_not_loaded}.
+
+-spec get_string_field(msgRef(), tagNum()) -> {ok, string()} | {error, reason()}.
+get_string_field(_MsgRef, _TagNum) ->
+   {error, library_not_loaded}.
+
+-spec get_char_field(msgRef(), tagNum()) -> {ok, char()} | {error, reason()}.
+get_char_field(_MsgRef, _TagNum) ->
    {error, library_not_loaded}.
