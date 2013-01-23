3789e9c63380789d8b8acd26da37c93d777b9b48
Dmitry Melnikov <dmitryme@gmail.com>
2013-01-23 17:23:39 +0400

    Update src/fix_parser.erl

--- src/fix_parser.erl
+++ src/fix_parser.erl
@@ -1,55 +1,35 @@
 -module(fix_parser).
 
+-include("erlang_fix.hrl").
+
 -export([create/3, create_msg/2, add_group/2, get_group/3, del_group/3,
-      set_int32_field/3, set_int64_field/3, set_double_field/3, set_string_field/3, set_char_field/3,
-      get_int32_field/2, get_int64_field/2, get_double_field/2, get_string_field/2, get_char_field/2,
-      msg_to_fix/2, fix_to_msg/3]).
+      set_int32_field/3, set_int64_field/3, set_double_field/3, set_string_field/3, set_char_field/3, set_data_field/3,
+      get_int32_field/2, get_int64_field/2, get_double_field/2, get_string_field/2, get_char_field/2, get_data_field/2,
+      msg_to_str/2, str_to_msg/3]).
 
 -on_load(load_lib/0).
-
--type attr()      :: {'page_size', pos_integer()} |
-                     {'max_page_size', pos_integer()} |
-                     {'num_pages', pos_integer()} |
-                     {'max_pages', pos_integer()} |
-                     {'num_groups', pos_integer()} |
-                     {'max_groups', pos_integer()}.  %% parser attributes ...
--type attrs()     :: [attr()].
--type flag()      :: check_crc |
-                     check_required |
-                     check_value |
-                     check_unknown_fields |
-                     check_all.
--type flags()     :: [flag()].
--type parserRef() :: {reference(), binary()}.
--type msgRef()    :: {reference(), binary(), binary()}.
--type groupRef()  :: {reference(), binary(), binary(), binary()}.
--type ref()       :: msgRef() | groupRef().
--type tagNum()    :: pos_integer().
--type reason()    :: atom() |
-                     string() |
-                     {pos_integer(), string()}.
 
 load_lib() ->
    erlang:load_nif(code:priv_dir(erlang_fix) ++ "/fix_parser", 0).
 
--spec create(string(), attrs(), flags()) -> {ok, parserRef()} | {error, reason()}.
+-spec create(string(), attrs(), flags()) -> {ok, #parser{}} | {error, reason()}.
 %% @doc creates new parser instance
 create(_Path, _Attrs, _Flags) ->
    {error, library_not_loaded}.
 
--spec create_msg(parserRef(), string()) -> {ok, msgRef()} | {error, reason()}.
+-spec create_msg(#parser{}, string()) -> {ok, #msg{}} | {error, reason()}.
 create_msg(_ParserRef, _MsgType) ->
    {error, library_not_loaded}.
 
--spec add_group(ref(), tagNum()) -> {ok, groupRef()} | {error, reason()}.
+-spec add_group(ref(), tagNum()) -> {ok, #group{}} | {error, reason()}.
 add_group(_Ref, _TagNum) ->
    {error, library_not_loaded}.
 
--spec get_group(ref(), tagNum(), pos_integer()) -> {ok, groupRef()} | {error, reason()}.
+-spec get_group(ref(), tagNum(), pos_integer()) -> {ok, #group{}} | {error, reason()}.
 get_group(_Ref, _TagNum, _Idx) ->
    {error, library_not_loaded}.
 
--spec del_group(ref(), tagNum(), pos_integer()) -> {ok, groupRef()} | {error, reason()}.
+-spec del_group(ref(), tagNum(), pos_integer()) -> ok | {error, reason()}.
 del_group(_Ref, _TagNum, _Idx) ->
    {error, library_not_loaded}.
 
@@ -73,6 +53,10 @@ set_string_field(_MsgRef, _TagNum, _Value) ->
 set_char_field(_MsgRef, _TagNum, _Value) ->
    {error, library_not_loaded}.
 
+-spec set_data_field(ref(), tagNum(), binary()) -> ok | {error, reason()}.
+set_data_field(_MsgRef, _TagNum, _Value) ->
+   {error, library_not_loaded}.
+
 -spec get_int32_field(ref(), tagNum()) -> {ok, integer()} | {error, reason()}.
 get_int32_field(_MsgRef, _TagNum) ->
    {error, library_not_loaded}.
@@ -93,10 +77,14 @@ get_string_field(_MsgRef, _TagNum) ->
 get_char_field(_MsgRef, _TagNum) ->
    {error, library_not_loaded}.
 
--spec msg_to_fix(msgRef(), char()) -> {ok, char()} | {error, reason()}.
-msg_to_fix(_MsgRef, _Delimiter) ->
+-spec get_data_field(ref(), tagNum()) -> {ok, binary()} | {error, reason()}.
+get_data_field(_MsgRef, _TagNum) ->
    {error, library_not_loaded}.
 
--spec fix_to_msg(parserRef(), char(), binary()) -> {ok, msgRef(), binary()} | {error, reason()}.
-fix_to_msg(_ParserRef, _Delimiter, _BinData) ->
+-spec msg_to_str(#msg{}, char()) -> {ok, char()} | {error, reason()}.
+msg_to_str(_MsgRef, _Delimiter) ->
+   {error, library_not_loaded}.
+
+-spec str_to_msg(#parser{}, char(), binary()) -> {ok, #msg{}, binary()} | {error, reason()}.
+str_to_msg(_ParserRef, _Delimiter, _BinData) ->
    {error, library_not_loaded}.
