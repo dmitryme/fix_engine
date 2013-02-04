158144ebdcb36fd6998a8a5aa491ffd35336cfec
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-04 18:53:59 +0400

    Update src/gen_fix_session.erl

--- src/gen_fix_session.erl
+++ src/gen_fix_session.erl
@@ -1,10 +1,12 @@
 -module(gen_fix_session).
 
+-include("fix_engine_config.hrl").
+
 -behaviour(gen_server).
 
 -export([send_logon/4]).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start_link/1, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {}).
 
@@ -15,6 +17,14 @@ send_logon(SessionID, Socket, Logon, Timeout) ->
       {'EXIT', {Reason, _}} ->
          {error, Reason}
    end.
+
+start_link(Args = #fix_session_acceptor_config{senderCompID = SenderCompID, targetCompID = TargetCompID}) ->
+   SessionID = fix_utils:make_session_id(SenderCompID, TargetCompID),
+   gen_server:start_link({local, SessionID}, ?MODULE, Args);
+
+start_link(Args = #fix_session_initiator_config{senderCompID = SenderCompID, targetCompID = TargetCompID}) ->
+   SessionID = fix_utils:make_session_id(SenderCompID, TargetCompID),
+   gen_server:start_link({local, SessionID}, ?MODULE, Args).
 
 init(_Args) ->
    {ok, #state{}}.
