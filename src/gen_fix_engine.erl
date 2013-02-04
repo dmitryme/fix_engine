63e09a2760138461bc77ce080e6db1f23ab0f2ee
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-04 18:53:32 +0400

    Update src/gen_fix_engine.erl

--- src/gen_fix_engine.erl
+++ src/gen_fix_engine.erl
@@ -4,9 +4,12 @@
 
 -behaviour(gen_server).
 
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([start_link/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {supervisor, acceptors, initiators}).
+
+start_link(Name, Args) ->
+   gen_server:start_link(Name, ?MODULE, Args, []).
 
 -spec init(#fix_engine_config{}) -> {ok, term()}.
 init(Args = #fix_engine_config{}) ->
@@ -70,3 +73,24 @@ create_sessions([Session = #fix_session_initiator_config{}|Rest], Supervisor, Id
             Session#fix_session_initiator_config.senderCompID,
             Session#fix_session_initiator_config.targetCompID), Child}),
    create_sessions(Rest, Supervisor, Id + 1, Acceptors, Initiators).
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+test1_test() ->
+   {ok, Supervisor} = supervisor:start_link(erlang_fix_sup, []),
+   Acceptors = ets:new(acceptors, []),
+   Initiators = ets:new(initiators, []),
+   Sessions = [
+         #fix_session_acceptor_config{
+            module = gen_fix_session,
+            senderCompID = "SERVER1",
+            targetCompID = "CLIENT1",
+            username = "user1",
+            password = "passwd",
+            fix_protocol = "fix4.4.xml"
+         }],
+   create_sessions(Sessions, Supervisor, 0, Acceptors, Initiators).
+
+-endif.
