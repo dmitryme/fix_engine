ed4eced47ef9b6643b61cfd5d1b52b06f863d6f5
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-06 18:35:20 +0400

    Update apps/fix_engine/src/gen_fix_engine.erl

--- apps/fix_engine/src/gen_fix_engine.erl
+++ apps/fix_engine/src/gen_fix_engine.erl
@@ -6,18 +6,19 @@
 
 -export([start_link/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {supervisor, acceptors, initiators}).
+-record(state, {supervisor, socket, inet_async_acc, acceptors, initiators}).
 
 start_link(Name, Args) ->
    gen_server:start_link(Name, ?MODULE, Args, []).
 
 -spec init(#fix_engine_config{}) -> {ok, term()}.
-init(Args = #fix_engine_config{}) ->
-   {ok, Supervisor} = supervisor:start_link(erlang_fix_sup, []),
+init(#fix_engine_config{sessions = Sessions, listenPort = ListenPort}) ->
+   {ok, Supervisor} = supervisor:start_link(fix_engine_sup, []),
    Acceptors = ets:new(acceptors, []),
    Initiators = ets:new(initiators, []),
-   ok = create_sessions(Args#fix_engine_config.sessions, Supervisor, 0, Acceptors, Initiators),
-   {ok, #state{supervisor = Supervisor, acceptors = Acceptors, initiators = Initiators}}.
+   {Socket, InetAsyncAccRef} = open_socket(ListenPort),
+   ok = create_sessions(Sessions, Supervisor, 0, Acceptors, Initiators),
+   {ok, #state{supervisor = Supervisor, socket = Socket, inet_async_acc = InetAsyncAccRef, acceptors = Acceptors, initiators = Initiators}}.
 
 handle_call(_Request, _From, State) ->
    {reply, ok, State}.
@@ -34,15 +35,25 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
    {ok, State}.
 
--spec create_sessions([#fix_session_initiator_config{}|#fix_session_acceptor_config{}], pid(), pos_integer(), term(),
-   term()) -> ok.
+open_socket(ListenPort) when is_number(ListenPort) ->
+   {ok, ListenSocket} = gen_tcp:listen(ListenPort, [{active, false}, binary, {reuseaddr, true}, {keepalive, true},
+         {backlog, 30}]),
+   {ok, Ref} = prim_inet:async_accept(ListenSocket, -1),
+   Ref;
+open_socket(ListenPort) ->
+   error_logger:info_msg("Listen port '~p' not opened.", [ListenPort]).
+
+
+-spec create_sessions([#fix_session_initiator_config{}|#fix_session_acceptor_config{}], pid(), pos_integer(), term(), term()) -> ok.
 create_sessions([], _Supervisor, 0, _, _) ->
    error_logger:warning_msg("No sessions are configured."),
    ok;
+
 create_sessions([], _Supervisor, _Id, _, _) ->
    ok;
+
 create_sessions([Session = #fix_session_acceptor_config{}|Rest], Supervisor, Id, Acceptors, Initiators) ->
-   {ok, Child} = supervisor:start_child(
+   {ok, ChildPid} = supervisor:start_child(
       Supervisor, {
          Id,
          {Session#fix_session_acceptor_config.module, start_link, [Session]},
@@ -55,10 +66,11 @@ create_sessions([Session = #fix_session_acceptor_config{}|Rest], Supervisor, Id,
       Acceptors, {
          fix_utils:make_session_id(
             Session#fix_session_acceptor_config.senderCompID,
-            Session#fix_session_acceptor_config.targetCompID), Child}),
+            Session#fix_session_acceptor_config.targetCompID), ChildPid}),
    create_sessions(Rest, Supervisor, Id + 1, Acceptors, Initiators);
+
 create_sessions([Session = #fix_session_initiator_config{}|Rest], Supervisor, Id, Acceptors, Initiators) ->
-   {ok, Child} = supervisor:start_child(
+   {ok, ChildPid} = supervisor:start_child(
       Supervisor, {
          Id,
          {Session#fix_session_initiator_config.module, start_link, [Session]},
@@ -71,26 +83,5 @@ create_sessions([Session = #fix_session_initiator_config{}|Rest], Supervisor, Id
       Initiators, {
          fix_utils:make_session_id(
             Session#fix_session_initiator_config.senderCompID,
-            Session#fix_session_initiator_config.targetCompID), Child}),
+            Session#fix_session_initiator_config.targetCompID), ChildPid}),
    create_sessions(Rest, Supervisor, Id + 1, Acceptors, Initiators).
-
--ifdef(TEST).
-
--include_lib("eunit/include/eunit.hrl").
-
-test1_test() ->
-   {ok, Supervisor} = supervisor:start_link(erlang_fix_sup, []),
-   Acceptors = ets:new(acceptors, []),
-   Initiators = ets:new(initiators, []),
-   Sessions = [
-         #fix_session_acceptor_config{
-            module = gen_fix_session,
-            senderCompID = "SERVER1",
-            targetCompID = "CLIENT1",
-            username = "user1",
-            password = "passwd",
-            fix_protocol = "fix4.4.xml"
-         }],
-   create_sessions(Sessions, Supervisor, 0, Acceptors, Initiators).
-
--endif.
