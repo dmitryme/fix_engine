fc305af778b9e5710d5509b12648bd3474691d10
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-08 14:17:47 +0400

    Update apps/fix_engine/src/gen_fix_engine.erl

--- apps/fix_engine/src/gen_fix_engine.erl
+++ apps/fix_engine/src/gen_fix_engine.erl
@@ -6,19 +6,27 @@
 
 -export([start_link/2, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {supervisor, socket, inet_async_acc, acceptors, initiators}).
+-record(state, {supervisor, socket, inet_async_ref, acceptors, initiators}).
 
 start_link(Name, Args) ->
    gen_server:start_link(Name, ?MODULE, Args, []).
 
 -spec init(#fix_engine_config{}) -> {ok, term()}.
+init(#fix_engine_config{sessions = Sessions, listenPort = undefined}) ->
+   {Supervisor, Acceptors, Initiators} = init_common(Sessions),
+   {ok, #state{supervisor = Supervisor, acceptors = Acceptors, initiators = Initiators}};
 init(#fix_engine_config{sessions = Sessions, listenPort = ListenPort}) ->
+   {Supervisor, Acceptors, Initiators} = init_common(Sessions),
+   {Socket, InetAsyncRef} = open_socket(ListenPort),
+   {ok, #state{supervisor = Supervisor, socket = Socket, inet_async_ref = InetAsyncRef, acceptors = Acceptors, initiators = Initiators}}.
+
+init_common(Sessions) ->
+   error_logger:info_msg("gen_fix_engine init started"),
    {ok, Supervisor} = supervisor:start_link(fix_engine_sup, []),
    Acceptors = ets:new(acceptors, []),
    Initiators = ets:new(initiators, []),
-   {Socket, InetAsyncAccRef} = open_socket(ListenPort),
    ok = create_sessions(Sessions, Supervisor, 0, Acceptors, Initiators),
-   {ok, #state{supervisor = Supervisor, socket = Socket, inet_async_acc = InetAsyncAccRef, acceptors = Acceptors, initiators = Initiators}}.
+   {Supervisor, Acceptors, Initiators}.
 
 handle_call(_Request, _From, State) ->
    {reply, ok, State}.
@@ -26,6 +34,16 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Request, State) ->
    {noreply, State}.
 
+handle_info({inet_async, ListenSocket, _Ref, {ok, ClientSocket}}, State) ->
+   case inet:peername(ClientSocket) of
+      {ok, PeerInfo} -> error_logger:info_msg("New connection ~p accepted.", [PeerInfo]);
+      {error, ErrCode} -> exit({peername_error, ErrCode})
+   end,
+   case prim_inet:async_accept(ListenSocket, -1) of
+      {ok, NewRef} -> ok;
+      {error, NewRef} -> exit({async_accept, inet:format_error(NewRef)})
+   end,
+   {noreply, State#state{inet_async_ref = NewRef}};
 handle_info(_Info, State) ->
    {noreply, State}.
 
@@ -36,10 +54,14 @@ code_change(_OldVsn, State, _Extra) ->
    {ok, State}.
 
 open_socket(ListenPort) when is_number(ListenPort) ->
+   error_logger:info_msg("Opening port ~p.", [ListenPort]),
    {ok, ListenSocket} = gen_tcp:listen(ListenPort, [{active, false}, binary, {reuseaddr, true}, {keepalive, true},
          {backlog, 30}]),
-   {ok, Ref} = prim_inet:async_accept(ListenSocket, -1),
-   Ref;
+   case prim_inet:async_accept(ListenSocket, -1) of
+      {ok, Ref} -> ok;
+      {error, Ref} -> exit({async_accept, inet:format_error(Ref)})
+   end,
+   {ListenSocket, Ref};
 open_socket(ListenPort) ->
    error_logger:info_msg("Listen port '~p' not opened.", [ListenPort]).
 
