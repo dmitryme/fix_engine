b98034c97ca930ca818eec4e4485e8898228d7c1
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-21 18:51:40 +0400

    Update apps/example/src/example_sup.erl

--- apps/example/src/example_sup.erl
+++ apps/example/src/example_sup.erl
@@ -25,6 +25,5 @@ start_link() ->
 init([]) ->
    {ok, {{one_for_one, 5, 10},
          [
-            ?CHILD(fix_server, worker),
-            ?CHILD(fix_client, worker)
+            ?CHILD(fix_engine, worker)
          ]}}.
