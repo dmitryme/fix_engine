e2405ccc6cc68a6cb0d8a050ca30eb86eb636208
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-21 18:48:54 +0400

    Update apps/fix_engine/include/fix_engine_config.hrl

--- apps/fix_engine/include/fix_engine_config.hrl
+++ apps/fix_engine/include/fix_engine_config.hrl
@@ -1,27 +1,32 @@
 -record(fix_session_acceptor_config,
    {
-      module :: atom(),
-      senderCompID       :: string(),
-      targetCompID       :: string(),
-      username      = [] :: string(),
-      password      = [] :: string(),
-      fix_protocol       :: string()
+      module                           :: atom(),
+      senderCompID                     :: string(),
+      targetCompID                     :: string(),
+      username          = []           :: string(),
+      password          = []           :: string(),
+      fix_protocol                     :: string(),
+      useTracer         = false        :: boolean(),
+      fix_parser_flags  = [check_all]  :: [atom()]
    }).
 
 -record(fix_session_initiator_config,
    {
-      module :: atom(),
-      senderCompID  :: string(),
-      targetCompID  :: string(),
-      username = [] :: string(),
-      password = [] :: string(),
-      fix_protocol  :: string(),
-      host          :: string(),
-      port          :: pos_integer()
+      module                           :: atom(),
+      senderCompID                     :: string(),
+      targetCompID                     :: string(),
+      username      = []               :: string(),
+      password      = []               :: string(),
+      fix_protocol                     :: string(),
+      host                             :: string(),
+      port                             :: pos_integer(),
+      useTracer     = false            :: boolean(),
+      fix_parser_flags = [check_all]   :: [atom()]
    }).
 
 -record(fix_engine_config,
    {
-      listenPort :: pos_integer(),
-      sessions :: [#fix_session_acceptor_config{}|#fix_session_initiator_config{}]
+      listenPort        :: pos_integer(),
+      tracerDir   = "." :: string(),
+      sessions          :: [#fix_session_acceptor_config{}|#fix_session_initiator_config{}]
    }).
