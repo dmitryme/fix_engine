c3fe6bb7b0da808c24f93bfcdeacf4790f1d5886
Dmitry Melnikov <dmitryme@gmail.com>
2013-02-21 18:49:35 +0400

    Update apps/fix_engine/src/fix_utils.erl

--- apps/fix_engine/src/fix_utils.erl
+++ apps/fix_engine/src/fix_utils.erl
@@ -1,6 +1,6 @@
 -module(fix_utils).
 
--export([list_to_atom/1, get_property/2, get_property/3, make_session_id/2, now_utc/0]).
+-export([list_to_atom/1, get_property/2, get_property/3, make_session_id/2, now_utc/0, now/0]).
 
 list_to_atom(List) ->
    try erlang:list_to_existing_atom(List) of
@@ -26,11 +26,17 @@ get_property(Key, PropList, DefValue) ->
    end.
 
 make_session_id(SenderCompID, TargetCompID) ->
-   SessionID = SenderCompID ++ "@" ++ TargetCompID,
+   SessionID = SenderCompID ++ "_" ++ TargetCompID,
    ?MODULE:list_to_atom(SessionID).
 
 now_utc() ->
    Now = {_, _, MicroSec} = erlang:now(),
    {{YYYY, M, DD},{HH, MM, SS}} = calendar:now_to_universal_time(Now),
-   lists:flatten(io_lib:format("~2.10.0B.~2.10.0B.~4.10.0B-~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0B",
-         [DD, M, YYYY, HH, MM, SS, MicroSec div 1000])).
+   lists:flatten(io_lib:format("~4.10.0B~2.10.0B~2.10.0B-~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0B",
+         [YYYY, M, DD, HH, MM, SS, MicroSec div 1000])).
+
+now() ->
+   Now = {_, _, MicroSec} = erlang:now(),
+   {{YYYY, M, DD},{HH, MM, SS}} = calendar:now_to_local_time(Now),
+   lists:flatten(io_lib:format("~4.10.0B~2.10.0B~2.10.0B-~2.10.0B:~2.10.0B:~2.10.0B.~3.10.0B",
+         [YYYY, M, DD, HH, MM, SS, MicroSec div 1000])).
