878d6df40f187a62ea520dec84c23a792a59f27d
Dmitry Melnikov <dmitryme@gmail.com>
2013-03-01 18:42:50 +0400

    Update gen_fix_acceptor_SUITE.erl

--- apps/fix_engine/test/gen_fix_acceptor_SUITE.erl
+++ apps/fix_engine/test/gen_fix_acceptor_SUITE.erl
@@ -6,7 +6,45 @@
 
 -compile([export_all]).
 
-all() -> [connect_disconnect_test, bad_login_test].
+receive_all(Timeout, Acc) ->
+   receive
+      Msg ->
+         receive_all(Timeout, [Msg|Acc])
+   after
+      Timeout ->
+         lists:reverse(Acc)
+   end.
+
+expected(Timeout, PatternList) ->
+   Msgs = receive_all(Timeout, []),
+   case (length(Msgs) == length(PatternList)) of
+      true ->
+         check(Msgs, PatternList, 1);
+      false ->
+         ct:fail("Msgs =/= Patterns")
+   end.
+
+check([], [], _) ->
+   ok;
+check([Msg|Msgs], [Pattern|PatternList], Counter) ->
+   case (catch Pattern(Msg)) of
+      {'EXIT', {function_clause, _}} ->
+         ct:fail("Message [~p] doesn't match to ~p pattern", [Msg, Counter]);
+      failed ->
+         ok;
+      ok ->
+         check(Msgs, PatternList, Counter + 1);
+      Ret ->
+         ct:fail("Pattern ~p unknown return. Should be at least 'ok'", [Counter])
+   end.
+
+
+all() ->
+   [disconnected_connect_test,
+    disconnected_logon_test,
+    disconnected_bad_msg_test,
+    connected_timeout_test,
+    connected_bad_msg_test].
 
 init_per_suite(Config) ->
    Config.
@@ -32,22 +70,58 @@ init_per_testcase(TestCase, Config) ->
 end_per_testcase(TestCase, Config) ->
    Config.
 
-connect_disconnect_test(_Config) ->
+disconnected_connect_test(_Config) ->
    gen_fix_acceptor:connect(server_client),
+   'CONNECTED' == gen_fix_acceptor:get_current_state(server_client),
    gen_fix_acceptor:disconnect(server_client),
+   'DISCONNECTED' == gen_fix_acceptor:get_current_state(server_client),
    ok.
 
-bad_login_test([{parser_ref, ParserRef}, {session_pid, SessionPid} | Config]) ->
-   {ok, Logon} = fix_parser:create_msg(ParserRef, "8"),
-   {ok, BinLogon} = fix_parser:msg_to_binary(Logon, ?FIX_SOH),
-   SessionPid ! {tcp, self(), BinLogon},
-   receive
-      tcp_close ->
-         ok;
-      Msg ->
-         ct:fail("Wrong message [~p] received.", [Msg])
-   after
-      1000 ->
-         ct:fail("close not received")
-   end,
+disconnected_logon_test(Config)->
+   SessionPid = proplists:get_value(session_pid, Config),
+   LogonBin = <<"8=FIX.4.4\0019=72\00135=A\00149=client\00156=server\00134=1\00152=20130301-10:07:06.558\00198=0\001108=1\001141=Y\00110=010\001">>,
+   SessionPid ! {tcp, self(), LogonBin},
+   expected(1000,
+      [
+         fun(tcp_close) -> ok end
+      ]),
+   'DISCONNECTED' == gen_fix_acceptor:get_current_state(server_client),
+   ok.
+
+disconnected_bad_msg_test(Config) ->
+   SessionPid = proplists:get_value(session_pid, Config),
+   LogonBin = <<"8=FIX.4.4\0019=\00135=A">>,
+   SessionPid ! {tcp, self(), LogonBin},
+   expected(1000,
+      [
+         fun(tcp_close) -> ok end
+      ]),
+   'DISCONNECTED' == gen_fix_acceptor:get_current_state(server_client),
+   ok.
+
+connected_timeout_test(Config) ->
+   gen_fix_acceptor:connect(server_client),
+   'CONNECTED' == gen_fix_acceptor:get_current_state(server_client),
+   SessionPid = proplists:get_value(session_pid, Config),
+   SessionPid ! {timeout, self(), heartbeat},
+   expected(1000, []),
+   'CONNECTED' == gen_fix_acceptor:get_current_state(server_client),
+   ok.
+
+connected_bad_msg_test(Config) ->
+   ParserRef = proplists:get_value(parser_ref, Config),
+   gen_fix_acceptor:connect(server_client),
+   'CONNECTED' == gen_fix_acceptor:get_current_state(server_client),
+   SessionPid = proplists:get_value(session_pid, Config),
+   LogonBin = <<"8=FIX.4.4\0019=\00135=A">>,
+   SessionPid ! {tcp, self(), LogonBin},
+   expected(1000,
+      [
+         fun(Bin) when is_binary(Bin) ->
+            {ok, Msg = #msg{type = "5"}, _} = fix_parser:binary_to_msg(ParserRef, ?FIX_SOH, Bin),
+            ok
+         end,
+         fun(tcp_close) -> ok end
+      ]),
+   'CONNECTED' == gen_fix_acceptor:get_current_state(server_client),
    ok.
