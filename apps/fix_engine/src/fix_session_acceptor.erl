36c07df49e42275559fba1cb32c1410a910dffa2
Dmitry Melnikov <dmitryme@gmail.com>
2013-03-04 18:30:24 +0400

    Update fix_session_acceptor.erl

--- apps/fix_engine/src/fix_session_acceptor.erl
+++ apps/fix_engine/src/fix_session_acceptor.erl
@@ -32,14 +32,7 @@ loop(Socket, Timeout, LogonPart) ->
             {ok, #msg_header{sender_comp_id = SenderCompID, target_comp_id = TargetCompID}} ->
                SessionID = fix_utils:make_session_id(TargetCompID,  SenderCompID),
                error_logger:info_msg("New incoming session [~p] detected.", [SessionID]),
-               case ets:lookup(fix_acceptors, SessionID) of
-                  [{SessionID, SessionPid}] ->
-                     ok = gen_fix_acceptor:set_socket(SessionPid, Socket),
-                     SessionPid ! {tcp, Socket, Logon};
-                  [] ->
-                     error_logger:error_msg("No such session [~p] configured.", [SessionID]),
-                     gen_tcp:close(Socket)
-               end;
+               accept_session(SessionID, Socket, Logon);
             {error, ?FIX_ERROR_BODY_TOO_SHORT, _} ->
                ?MODULE:loop(Socket, Timeout, Logon);
             {error, ErrCode, ErrText} ->
@@ -47,4 +40,19 @@ loop(Socket, Timeout, LogonPart) ->
                   [ErrCode, ErrText, Logon]),
                gen_tcp:close(Socket)
          end
+   end.
+
+accept_session(SessionID, Socket, Logon) ->
+   case ets:lookup(fix_acceptors, SessionID) of
+      [{SessionID, SessionPid}] ->
+         case gen_fix_acceptor:set_socket(SessionPid, Socket) of
+            ok ->
+               SessionPid ! {tcp, Socket, Logon};
+            {error, already_connected} ->
+               error_logger:error_msg("[~p]: socket already connected. Socket will be closed.", [SessionID]),
+               gen_tcp:close(Socket)
+         end;
+      [] ->
+         error_logger:error_msg("No such session [~p] configured.", [SessionID]),
+         gen_tcp:close(Socket)
    end.
