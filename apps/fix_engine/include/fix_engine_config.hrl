b7ee543436418efca70ed9f067430b1e4ea8f8e1
Dmitry Melnikov <dmitryme@gmail.com>
2013-03-07 19:01:29 +0400

    Update fix_engine_config.hrl

--- apps/fix_engine/include/fix_engine_config.hrl
+++ apps/fix_engine/include/fix_engine_config.hrl
@@ -14,14 +14,14 @@
       username             = []          :: string(),
       password             = []          :: string(),
       fix_protocol                       :: string(),
-      use_tracer           = false       :: boolean(),
-      tracer_dir           = undef       :: string(),
-      storade_dir          = undef       :: string(),        % will be derived from parent #fix_engine_config.storade_dir
-      storage_type         = undef       :: atom(),          % will be derived from parent #fix_engine_config.storade_type
-      storage_flags        = undef       :: [term()],        % will be derived from parent #fix_engine_config.storade_flags
+      tracer_type          = undef       :: atom(),        % will be derived from parent #fix_engine_config.storade_dir
+      tracer_dir           = undef       :: string(),      % will be derived from parent #fix_engine_config.storade_dir
+      storage_dir          = undef       :: string(),      % will be derived from parent #fix_engine_config.storade_dir
+      storage_type         = undef       :: atom(),        % will be derived from parent #fix_engine_config.storade_type
+      storage_flags        = undef       :: [term()],      % will be derived from parent #fix_engine_config.storade_flags
       fix_parser_flags     = [check_all] :: [atom()],
-      lost_heartbeat_delta = 500         :: pos_integer()    % in milliseconds. Heartbeat considered lost if no messages
-                                                             % is received after HeartBtInt * 1000 + lost_heartbeat_delta
+      lost_heartbeat_delta = 500         :: pos_integer()  % in milliseconds. Heartbeat considered lost if no messages
+                                                           % is received after HeartBtInt * 1000 + lost_heartbeat_delta
    }).
 
 -record(fix_session_initiator_config,
@@ -35,9 +35,9 @@
       fix_protocol                       :: string(),
       host                               :: string(),
       port                               :: pos_integer(),
-      use_tracer           = false       :: boolean(),
-      tracer_dir           = undef       :: string(),
-      storade_dir          = undef       :: string(),        % will be derived from parent #fix_engine_config.storade_dir
+      tracer_type          = undef       :: atom(),          % will be derived from parent #fix_engine_config.storade_dir
+      tracer_dir           = undef       :: string(),        % will be derived from parent #fix_engine_config.storade_dir
+      storage_dir          = undef       :: string(),        % will be derived from parent #fix_engine_config.storade_dir
       storage_type         = undef       :: atom(),          % will be derived from parent #fix_engine_config.storade_type
       storage_flags        = undef       :: [term()],        % will be derived from parent #fix_engine_config.storade_flags
       fix_parser_flags     = [check_all] :: [atom()],
@@ -50,9 +50,10 @@
 -record(fix_engine_config,
    {
       listen_port                            :: pos_integer(),
+      tracer_type       = null               :: null | file,
       tracer_dir        = "."                :: string(),
-      storade_dir       = "."                :: string(),
-      storage_type      = dets               :: atom(),         % dets, ets, null
+      storage_dir       = "."                :: string(),
+      storage_type      = dets               :: dets | ets | null,
       storage_flags     = []                 :: [term()],
       socket_opts       = ?def_socket_opts   :: [gen_tcp:option()],
       sessions                               :: [#fix_session_acceptor_config{}|#fix_session_initiator_config{}]
