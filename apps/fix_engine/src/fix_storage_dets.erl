77662ebc33a65fa40ba085a7b8644dd43b0e42b6
Dmitry Melnikov <dmitryme@gmail.com>
2013-03-14 18:58:00 +0400

    Update fix_storage_dets.erl

--- apps/fix_engine/src/fix_storage_dets.erl
+++ apps/fix_engine/src/fix_storage_dets.erl
@@ -5,26 +5,26 @@
 -behaviour(gen_server).
 
 -export([start_link/1, init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--export([reset/2, store/4]).
 
--record(state, {}).
-
-reset(Storage, SessionID) ->
-   gen_server:cast(Storage, {reset, SessionID}).
-
-store(Storage, SessionID, MsgSeqNum, Msg) ->
-   gen_server:cast(Storage, {store, SessionID, MsgSeqNum, Msg}).
+-record(state, {table_ref}).
 
 start_link(SessionCfg = #fix_session_config{storage = Storage}) ->
    gen_server:start_link({local, Storage}, ?MODULE, SessionCfg, []).
 
-init(_SessionCfg) ->
-   {ok, #state{}}.
+init(#fix_session_config{session_id = SessionID, storage_dir = Dir, storage_flags = Flags}) ->
+   error_logger:info_msg("FLAGS: ~p", [Flags]),
+   ok = fix_utils:make_dir(Dir),
+   FileName = filename:join([Dir, SessionID]) ++ ".storage",
+   TableName = fix_utils:list_to_atom("fix_storage_" ++ atom_to_list(SessionID) ++ "_table"),
+   {ok, TableRef} = dets:open_file(TableName, [{file, FileName} | Flags]),
+   error_logger:info_msg("[~p]: storage file '~p' has been opened.", [SessionID, FileName]),
+   {ok, #state{table_ref = TableRef}}.
 
 handle_call(_Request, _From, State) ->
    {reply, ok, State}.
 
-handle_cast(_Request, State) ->
+handle_cast({store, MsgSeqNum, Msg}, State) ->
+   ok = dets:insert(State#state.table_ref, {MsgSeqNum, Msg}),
    {noreply, State}.
 
 handle_info(_Info, State) ->
