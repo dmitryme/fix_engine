33798029c3d8c94a9bc345e61ea62758350ecfd7
Dmitry Melnikov <dmitryme@gmail.com>
2013-03-15 11:10:42 +0400

    Create fix_storage.erl

---
+++ apps/fix_engine/src/fix_storage.erl
@@ -0,0 +1,20 @@
+-module(fix_storage).
+
+-export([
+      reset/1,
+      store_msg_out/3,
+      store_seq_num_in/2,
+      get_stat/1
+      ]).
+
+reset(Storage) ->
+   gen_server:cast(Storage, reset).
+
+store_msg_out(Storage, MsgSeqNum, Msg) ->
+   gen_server:cast(Storage, {store, MsgSeqNum, Msg}).
+
+store_seq_num_in(Storage, MsgSeqNum) ->
+   gen_server:cast(Storage, {seq_num_in, MsgSeqNum}).
+
+get_stat(Storage) ->
+   get_server:call(Storage, get_stat).
