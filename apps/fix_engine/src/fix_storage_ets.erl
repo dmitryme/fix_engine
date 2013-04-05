78b565c58c39e6eda381d41dc2faef630b89c04d
Dmitry Melnikov <dmitryme@gmail.com>
2013-04-05 18:31:19 +0400

    Update fix_storage_ets.erl

--- apps/fix_engine/src/fix_storage_ets.erl
+++ apps/fix_engine/src/fix_storage_ets.erl
@@ -13,42 +13,34 @@ start_link(SessionCfg = #fix_session_config{storage = Storage}) ->
 
 init(#fix_session_config{session_id = SessionID, storage_dir = Dir, storage_flags = Flags}) ->
    ok = fix_utils:make_dir(Dir),
-   FileName = filename:join([Dir, SessionID]) ++ ".storage",
    TableName = fix_utils:list_to_atom("fix_storage_" ++ atom_to_list(SessionID) ++ "_table"),
-   {ok, TableRef} = dets:open_file(TableName, [{file, FileName} | Flags]),
-   error_logger:info_msg("[~p]: storage file '~p' has been opened.", [SessionID, FileName]),
-   case dets:lookup(TableRef, stat) of
-      [{stat, SeqNumIn, SeqNumOut}] ->
-         ok;
-      [] ->
-         SeqNumIn = 0,
-         SeqNumOut = 0,
-         ok = dets:insert(TableRef, {stat, SeqNumIn, SeqNumOut})
-   end,
-   {ok, #state{session_id = SessionID, table_ref = TableRef, seq_num_in = SeqNumIn, seq_num_out = SeqNumOut}}.
+   TableRef = ets:new(TableName, Flags),
+   error_logger:info_msg("[~p]: storage file '~p' has been created.", [SessionID, TableName]),
+   true = ets:insert(TableRef, {stat, 0, 0}),
+   {ok, #state{session_id = SessionID, table_ref = TableRef, seq_num_in = 0, seq_num_out = 0}}.
 
 handle_call(get_stat, _From, State) ->
    {reply, {ok, {State#state.seq_num_in, State#state.seq_num_out}}, State}.
 
 handle_cast(reset, State = #state{table_ref = TableRef}) ->
-   ok = dets:delete_all_objects(TableRef),
-   ok = dets:insert(TableRef, {stat, 0, 0}),
+   ok = ets:delete_all_objects(TableRef),
+   true = ets:insert(TableRef, {stat, 0, 0}),
    {noreply, State#state{seq_num_in = 0, seq_num_out = 0}};
 
 handle_cast({seq_num_in, MsgSeqNum}, State = #state{table_ref = TableRef, seq_num_out = SeqNumOut}) ->
-   ok = dets:insert(TableRef, {stat, MsgSeqNum, SeqNumOut}),
+   true = ets:insert(TableRef, {stat, MsgSeqNum, SeqNumOut}),
    {noreply, State#state{seq_num_in = MsgSeqNum}};
 
 handle_cast({store, MsgSeqNum, Type, Msg}, State = #state{table_ref = TableRef, seq_num_in = SeqNumIn, seq_num_out = SeqNumOut}) ->
-   ok = dets:insert(TableRef, {MsgSeqNum, Type, Msg}),
-   ok = dets:insert(TableRef, {stat, SeqNumIn, SeqNumOut}),
+   true = ets:insert(TableRef, {MsgSeqNum, Type, Msg}),
+   true = ets:insert(TableRef, {stat, SeqNumIn, SeqNumOut}),
    {noreply, State#state{seq_num_out = MsgSeqNum}};
 
 handle_cast({resend, From, BeginSeqNo, EndSeqNo},
       State = #state{session_id = SessionID, table_ref = TableRef, seq_num_out = SeqNumOut}) ->
    EndSeqNo1 = if (EndSeqNo == 0) -> SeqNumOut; true -> EndSeqNo end,
    error_logger:info_msg("[~p]: try to find messages [~p,~p].", [SessionID, BeginSeqNo, EndSeqNo1]),
-   UnorderedMsgs = dets:select(TableRef,  [{{'$1', '_', '_'}, [
+   UnorderedMsgs = ets:select(TableRef,  [{{'$1', '_', '_'}, [
                {'>=', '$1', BeginSeqNo},{'=<', '$1', EndSeqNo1}, {'=/=', '$1', stat}], ['$_']}]),
    Msgs = lists:sort(UnorderedMsgs),
    error_logger:info_msg("[~p]: ~p messages will be resent.", [SessionID, length(Msgs)]),
@@ -59,7 +51,7 @@ handle_info(_Info, State) ->
    {noreply, State}.
 
 terminate(_Reason, #state{table_ref = TableRef}) ->
-   ok = dets:close(TableRef),
+   ok = ets:delete(TableRef),
    ok.
 
 code_change(_OldVsn, State, _Extra) ->
