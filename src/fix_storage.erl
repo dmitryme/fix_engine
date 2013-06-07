37ff3a3aa16cf8bff68155231729c7cc95da9485
Dmitry Melnikov <dmitryme@gmail.com>
2013-06-07 18:16:29 +0400

    Update fix_storage.erl

--- src/fix_storage.erl
+++ src/fix_storage.erl
@@ -5,7 +5,8 @@
       store_msg_out/4,
       store_seq_num_in/2,
       get_stat/1,
-      resend/3
+      resend/3,
+      get_metadata/2
       ]).
 
 reset(Storage) ->
@@ -17,8 +18,8 @@ store_msg_out(Storage, MsgSeqNum, Type, Msg) ->
 store_seq_num_in(Storage, MsgSeqNum) ->
    gen_server:cast(Storage, {seq_num_in, MsgSeqNum}).
 
-get_stat(Storage) ->
-   gen_server:call(Storage, get_stat).
-
 resend(Storage, BeginSeqNo, EndSeqNo) ->
    gen_server:cast(Storage, {resend, self(), BeginSeqNo, EndSeqNo}).
+
+get_metadata(Storage, Item) ->
+   gen_server:call(Storage, {get_metadata, Item}).
